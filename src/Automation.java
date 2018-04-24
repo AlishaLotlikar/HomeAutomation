eaa1f7b47d8c69c383c1d106af7a651ae6f06368
Student <Student@acer-PC>
2018-04-24 12:42:45 +0530

    commit

---
+++ src/Automation.java
@@ -0,0 +1,13 @@
+
+public class Automation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		tv myTv=new tv();
+		AC myAc=new AC();
+		lights light=new lights();
+		myTv.changeChannel(23);
+		
+	}
+
+}
---
+++ src/TestHAS.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestHAS {
+	tv tvOn=new tv();
+	@Test
+	public void switchOnTV() {
+		
+		tvOn.switchOnOffTV();
+	}
+	
+}
